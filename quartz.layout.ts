e1df22170c9cc98a1bac9096cb67fc24bf62971f
Ace-Extrenof <prateeshAce@proton.me>
2024-10-12 20:36:13 +0530

    giscus added

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,48 +1,65 @@
-import { PageLayout, SharedLayout } from "./quartz/cfg"
-import * as Component from "./quartz/components"
+import { PageLayout, SharedLayout } from "./quartz/cfg";
+import * as Component from "./quartz/components";
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
-  head: Component.Head(),
-  header: [],
-  afterBody: [],
-  footer: Component.Footer({
-    links: {
-    },
-  }),
-}
+    head: Component.Head(),
+    header: [],
+    afterBody: [
+        Component.Comments({
+            provider: "giscus",
+            options: {
+                // from data-repo
+                repo: "jackyzha0/quartz",
+                // from data-repo-id
+                repoId: "MDEwOlJlcG9zaXRvcnkzODcyMTMyMDg",
+                // from data-category
+                category: "Announcements",
+                // from data-category-id
+                categoryId: "DIC_kwDOFxRnmM4B-Xg6",
+            },
+        }),
+    ],
+    footer: Component.Footer({
+        links: {},
+    }),
+};
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
-  beforeBody: [
-    Component.Breadcrumbs(),
-    Component.ArticleTitle(),
-    Component.ContentMeta(),
-    Component.TagList(),
-  ],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Search(),
-    Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
-  ],
-  right: [
-    Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
-  ],
-}
+    beforeBody: [
+        Component.Breadcrumbs(),
+        Component.ArticleTitle(),
+        Component.ContentMeta(),
+        Component.TagList(),
+    ],
+    left: [
+        Component.PageTitle(),
+        Component.MobileOnly(Component.Spacer()),
+        Component.Search(),
+        Component.Darkmode(),
+        Component.DesktopOnly(Component.Explorer()),
+    ],
+    right: [
+        Component.Graph(),
+        Component.DesktopOnly(Component.TableOfContents()),
+        Component.Backlinks(),
+    ],
+};
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Search(),
-    Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
-  ],
-  right: [],
-}
+    beforeBody: [
+        Component.Breadcrumbs(),
+        Component.ArticleTitle(),
+        Component.ContentMeta(),
+    ],
+    left: [
+        Component.PageTitle(),
+        Component.MobileOnly(Component.Spacer()),
+        Component.Search(),
+        Component.Darkmode(),
+        Component.DesktopOnly(Component.Explorer()),
+    ],
+    right: [],
+};
